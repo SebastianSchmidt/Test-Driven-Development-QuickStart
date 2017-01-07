7823929ea99c5a2c047ff3408b23b57bbed30704
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-01-07 23:55:44 +0100

    Added JaCoCo.

--- src/main/java/io/github/sebastianschmidt/test_driven_development/quick_start/EntryPoint.java
+++ src/main/java/io/github/sebastianschmidt/test_driven_development/quick_start/EntryPoint.java
@@ -3,6 +3,10 @@ package io.github.sebastianschmidt.test_driven_development.quick_start;
 public class EntryPoint {
 
     public static void main(final String[] args) {
+        new EntryPoint().execute();
+    }
+
+    public void execute() {
         final HelloWorld instance = new HelloWorld();
         System.out.println(instance.getMessage());
     }
